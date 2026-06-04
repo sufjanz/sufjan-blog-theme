5050d5d6d1a274b6123efccc64c38cd41f25352b
sufjanz <sufjanzogaj@gmail.com>
2026-06-04 20:40:18 +0200

    1345678

---
+++ template-parts/content-archive.php
@@ -0,0 +1,10 @@
+<div class="container">
+    <div class="post mb-5">
+        <div class="mr-3 img-fluid post-thumb d-none d-md-flex" src="
+        <?php 
+            get_post_thumbnail_url('thumbnail');
+        ?>
+        " alt="image">
+    </div>
+    </div>
+</div>
